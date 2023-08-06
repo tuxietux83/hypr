7bfdfd729f3e5ddb10e1b56d007c910e05fb4dc0
tuxietux83 <66544011+tuxietux83@users.noreply.github.com>
2023-08-06 13:33:15 +0300

    Update hypr_download.sh

--- hypr_download.sh
+++ hypr_download.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ### Wayland latest
+if [ -d wayland ];then rm -rfv wayland ;fi 
 git clone https://gitlab.freedesktop.org/wayland/wayland.git
 cd wayland
 meson setup --prefix=/usr --buildtype=release -Ddocumentation=false build/ &&
@@ -9,6 +10,7 @@ sudo ninja -C build/ install
 cd ..
 
 ### Wayland-protocols latest
+if [ -d wayland-protocols ];then rm -rfv wayland-protocols ;fi
 git clone https://gitlab.freedesktop.org/wayland/wayland-protocols.git
 cd wayland-protocols
 meson setup --prefix=/usr --buildtype=release build/ &&
@@ -17,6 +19,7 @@ sudo ninja -C build/ install
 cd ..
 
 ### Libdisplay-info latest
+if [ -d libdisplay-info ];then rm -rfv libdisplay-info ;fi
 git clone https://gitlab.freedesktop.org/emersion/libdisplay-info.git
 cd libdisplay-info
 meson setup --prefix=/usr --buildtype=release build/ &&
@@ -25,6 +28,7 @@ sudo ninja -C build/ install
 cd ..
 
 ### Libliftoff latest
+if [ -d libliftoff ];then rm -rfv libliftoff ;fi
 git clone https://gitlab.freedesktop.org/emersion/libliftoff.git
 cd libliftoff
 meson setup --prefix=/usr --buildtype=release build/
@@ -33,6 +37,7 @@ sudo ninja -C build/ install
 cd ..
 
 # Libinput latest
+if [ -d libinput ];then rm -rfv libinput ;fi
 git clone https://gitlab.freedesktop.org/libinput/libinput
 cd libinput
 sudo apt build-dep libinput -y
@@ -42,6 +47,7 @@ sudo ninja -C build/ install
 cd ..
 
 ### Wlroots
+if [ -d wlroots ];then rm -rfv wlroots ;fi
 git clone https://gitlab.freedesktop.org/wlroots/wlroots.git
 cd wlroots
 meson setup --prefix=/usr --buildtype=release build/ &&
@@ -50,9 +56,10 @@ sudo ninja -C build/ install
 cd ..
 
 ### Hyprland latest
+if [ -d Hyprland ];then sudo rm -rfv Hyprland ;fi
 git clone --recursive https://github.com/hyprwm/Hyprland
 cd Hyprland
 meson subprojects update --reset
-meson setup build
-ninja -C build
-sudo ninja -C build install
+meson setup --prefix=/usr --buildtype=release -Ddocumentation=false build/
+ninja -C build/
+sudo ninja -C build/ install
