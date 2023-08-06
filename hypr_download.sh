ce1d625fedd5670d9a19153cf49be2e600ecd160
tuxietux83 <66544011+tuxietux83@users.noreply.github.com>
2023-08-06 10:44:22 +0300

    Add files via upload

---
+++ hypr_download.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+### Wayland latest
+git clone https://gitlab.freedesktop.org/wayland/wayland.git
+cd wayland
+mkdir build &&
+cd    build &&
+meson setup --prefix=/usr --buildtype=release \ -Ddocumentation=false &&
+ninja
+sudo ninja install
+cd ../..
+
+### Wayland-protocols latest
+git clone https://gitlab.freedesktop.org/wayland/wayland-protocols.git
+cd wayland-protocols
+mkdir build &&
+cd    build &&
+meson setup --prefix=/usr --buildtype=release &&
+ninja
+sudo ninja install
+cd ../..
+
+### Libdisplay-info latest
+git clone https://gitlab.freedesktop.org/emersion/libdisplay-info.git
+cd libdisplay-info
+mkdir build &&
+cd    build &&
+meson setup --prefix=/usr --buildtype=release &&
+ninja
+sudo ninja install
+cd ../..
+
+### Libliftoff latest
+git clone https://gitlab.freedesktop.org/emersion/libliftoff.git
+cd libliftoff
+meson setup build/
+ninja -C build/
+cd build
+sudo ninja install
+cd ../..
+### Hyprland latest
+git clone https://github.com/hyprwm/Hyprland.git
+cd Hyprland
+sudo make install
+cd ..
