ea84c490d8fd759ed8b85757454c8a4d1fa079fc
tuxietux83 <66544011+tuxietux83@users.noreply.github.com>
2023-08-06 14:44:50 +0300

    Update hypr_stage1.sh

--- hypr_stage1.sh
+++ hypr_stage1.sh
@@ -16,7 +16,7 @@ PKGLIST=(
 	libxcb-render-util0-dev libxcb-res0-dev libxcb-xinput-dev xdg-desktop-portal-wlr libpango1.0-dev\
 	hwdata libzfslinux-dev libgbm-dev freebsd-manpages edid-decode libsystemd-dev wayland-scanner++\
  	pixmap libc++-dev libgtk-4-dev valgrind qt6-wayland qtwayland5 pipewire wireplumber polkit-kde-agent-1\
-  	htop
+  	htop kitty
 )
 # Function to check if installed
 function IS_INSTALLED {
