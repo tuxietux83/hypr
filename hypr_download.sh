0d65328cc40c3cfbaccecfaf21b507bbdcbb011b
tuxietux83 <66544011+tuxietux83@users.noreply.github.com>
2023-08-06 13:44:47 +0300

    Update hypr_download.sh

--- hypr_download.sh
+++ hypr_download.sh
@@ -8,6 +8,7 @@ meson setup --prefix=/usr --buildtype=release -Ddocumentation=false build/ &&
 ninja -C build/
 sudo ninja -C build/ install
 cd ..
+read -p "wayland is installed, press enter to continue ..."
 
 ### Wayland-protocols latest
 if [ -d wayland-protocols ];then rm -rfv wayland-protocols ;fi
@@ -17,6 +18,7 @@ meson setup --prefix=/usr --buildtype=release build/ &&
 ninja -C build/
 sudo ninja -C build/ install
 cd ..
+read -p "wayland-protocols is installed, press enter to continue ..."
 
 ### Libdisplay-info latest
 if [ -d libdisplay-info ];then rm -rfv libdisplay-info ;fi
@@ -26,6 +28,7 @@ meson setup --prefix=/usr --buildtype=release build/ &&
 ninja -C build/
 sudo ninja -C build/ install
 cd ..
+read -p "libdisplay-info is installed, press enter to continue ..."
 
 ### Libliftoff latest
 if [ -d libliftoff ];then rm -rfv libliftoff ;fi
@@ -35,6 +38,7 @@ meson setup --prefix=/usr --buildtype=release build/
 ninja -C build/
 sudo ninja -C build/ install
 cd ..
+read -p "libliftoff is installed, press enter to continue ..."
 
 # Libinput latest
 if [ -d libinput ];then rm -rfv libinput ;fi
@@ -45,6 +49,7 @@ meson setup --prefix=/usr --buildtype=release -Ddocumentation=false build/
 ninja -C build/
 sudo ninja -C build/ install
 cd ..
+read -p "libinput is installed, press enter to continue ..."
 
 ### Wlroots
 if [ -d wlroots ];then rm -rfv wlroots ;fi
@@ -54,12 +59,13 @@ meson setup --prefix=/usr --buildtype=release build/ &&
 ninja -C build/
 sudo ninja -C build/ install
 cd ..
+read -p "wlroots is installed, press enter to continue ..."
 
 ### Hyprland latest
 if [ -d Hyprland ];then sudo rm -rfv Hyprland ;fi
 git clone --recursive https://github.com/hyprwm/Hyprland
 cd Hyprland
 meson subprojects update --reset
-meson setup --prefix=/usr --buildtype=release -Ddocumentation=false build/
+meson setup --prefix=/usr --buildtype=release build/
 ninja -C build/
 sudo ninja -C build/ install
