404c01c67be253e1dfea467c012c95e03b1d92fd
tuxietux83 <66544011+tuxietux83@users.noreply.github.com>
2023-08-13 13:55:10 +0300

    Delete hypr_v2 directory

--- hypr_v2/8_hyprland.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-### Hyprland latest
-if [ -d Hyprland ];then sudo rm -rfv Hyprland ;fi
-git clone --recursive https://github.com/hyprwm/Hyprland
-cd Hyprland
-sudo make install &&
-sudo cp example/hyprland.desktop /usr/share/wayland-sessions/
-cd ..
