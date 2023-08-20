54a2df7db135705c7fae63a0ac6ba28c581a136c
tuxietux83 <66544011+tuxietux83@users.noreply.github.com>
2023-08-20 15:13:15 +0300

    Update autostart.sh
    much nicer output

--- dot/bin/autostart.sh
+++ dot/bin/autostart.sh
@@ -1,58 +1,151 @@
 #!/usr/bin/bash
+#NOTE: Not tested on sway!!
+set -e -u
+# Colors
+default=$(tput sgr0)
+black=$(tput setaf 0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+blue=$(tput setaf 4)
+magenta=$(tput setaf 5)
+cyan=$(tput setaf 6)
+white=$(tput setaf 7)
+bold=$(tput bold)
+# Update SERVICE function
+SERVICE_UPDATE() {
+    SERVICE="$SERVICE"
+    SERVICE_INFO="[${green} $SERVICE${default} ]"
+    HYPR_SERVICE="[ $SERVICE ]"
+}
+# Vars
+SCTL_ACTIVE="systemctl --user is-active"
+SCTL_FAILED="systemctl --user is-failed"
+SCTL_STATUS="systemctl --user status"
+SCTL_START="systemctl --user start"
+SCTL_STOP="systemctl --user stop"
+SCTL_RESTART="systemctl --user restart"
+SCTL_INFO="[${cyan} systemctl${default} ]"
+HYPR_INFO="[ systemctl ]"
+INFO_ARROW="${yellow} ${default}"
+INFO_CLOSE="${red} ${default} "
+INFO_CHECK="${green} ${default} "
+NOTIFY="notify-send -u low -t 5000 -e -i dialog-information"
 
 # Session check for xdg-desktop-portals and session.target
 DESKTOP_SESSION=$(echo $DESKTOP_SESSION | tr '[:upper:]' '[:lower:]')
 case $DESKTOP_SESSION in
 	hyprland)
-		pkill -f xdg-desktop-portal*
-		systemctl --user stop session-*.target && systemctl --user start session-hyprland.target
-		echo "session-hyprland: => $(systemctl --user is-active session-hyprland.target)"
-		echo "hypr: => Killing portals"
-		sleep 2
-		systemctl --user start xdg-desktop-portal-hyprland.service
-		echo "portal: hyprland => $(systemctl --user is-active xdg-desktop-portal-hyprland.service)"
-		hyprctl notify -1 10000 "rgb(ff1ea3)" "xdg-desktop-portal-hyprland --> $(systemctl --user is-active xdg-desktop-portal-hyprland.service)"
-		systemctl --user start xdg-desktop-portal.service
-		echo "portal: portal => $(systemctl --user is-active xdg-desktop-portal.service)"
-		hyprctl notify -1 10000 "rgb(ff1ea3)" "xdg-desktop-portal-portal --> $(systemctl --user is-active xdg-desktop-portal.service)"
-		sleep 2
+		SERVICE="session.target"
+		SERVICE_UPDATE
+			echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW Stopping ...$INFO_CLOSE"
+			$($SCTL_STOP session-*.target) && $($SCTL_START session-hyprland.target)
+			echo "$SCTL_INFO$SERVICE_INFO$INFO_ARROW Restarting ...$INFO_CHECK"
+		SERVICE="xdg-desktop-portal*"
+		SERVICE_UPDATE
+			pgrep -f "$SERVICE" &>/dev/null &&\
+				pkill -f $SERVICE &&\
+				echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW Stopping ...$INFO_CLOSE"
+		SERVICE="xdg-desktop-portal-hyprland"
+		SERVICE_UPDATE
+		$($SCTL_START $SERVICE.service)
+		#sleep 1
+		$SCTL_ACTIVE $SERVICE.service &>/dev/null &&\
+			echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW $($SCTL_ACTIVE $SERVICE.service)$INFO_CHECK" &&\
+			hyprctl notify -0 10000 "rgb(6AA84F)" "$HYPR_INFO$HYPR_SERVICE -> $($SCTL_ACTIVE $SERVICE.service)" &>/dev/null
+		SERVICE="xdg-desktop-portal"
+		SERVICE_UPDATE
+		$($SCTL_START $SERVICE.service)
+		#sleep 1
+		$SCTL_ACTIVE $SERVICE.service &>/dev/null &&\
+			echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW $($SCTL_ACTIVE $SERVICE.service)$INFO_CHECK" &&\
+			hyprctl notify -0 10000 "rgb(6AA84F)" "$HYPR_INFO$HYPR_SERVICE -> $($SCTL_ACTIVE $SERVICE.service)" &>/dev/null
+		SERVICE="xdg-desktop-portal-gtk"
+		SERVICE_UPDATE
+		$SCTL_ACTIVE $SERVICE.service &>/dev/null &&\
+			echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW $($SCTL_ACTIVE $SERVICE.service)$INFO_CHECK" &&\
+			hyprctl notify -0 10000 "rgb(6AA84F)" "$HYPR_INFO$HYPR_SERVICE -> $($SCTL_ACTIVE $SERVICE.service)" &>/dev/null
 		dbus-update-activation-environment --systemd --all
 		;;
 	sway)
-		pkill -f xdg-desktop-portal*
-		systemctl --user stop session-*.target && systemctl --user start session-sway.target
-		echo "session-sway: => $(systemctl --user is-active session-sway.target)"
-		echo "sway: Killing portals"
-		sleep 2
-		systemctl --user start xdg-desktop-portal-wlr.service
-		echo "portal: wlr => $(systemctl --user is-active xdg-desktop-portal-wlr.service)"
-		notify-send -u low -t 10000 "xdg-desktop-portal-wlr --> $(systemctl --user is-active xdg-desktop-portal-wlr.service)"
-		systemctl --user start xdg-desktop-portal.service
-		echo "portal: portal => $(systemctl --user is-active xdg-desktop-portal.service)"
-		notify-send -u low -t 10000 "xdg-desktop-portal-portal --> $(systemctl --user is-active xdg-desktop-portal.service)"
-		sleep 2
+		SERVICE="session.target"
+		SERVICE_UPDATE
+			echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW Stopping ...$INFO_CLOSE"
+			$($SCTL_STOP session-*.target) && $($SCTL_START session-sway.target)
+			echo "$SCTL_INFO$SERVICE_INFO$INFO_ARROW Restarting ...$INFO_CHECK"
+		SERVICE="xdg-desktop-portal*"
+		SERVICE_UPDATE
+			pgrep -f "$SERVICE" &>/dev/null &&\
+				pkill -f $SERVICE &&\
+				echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW Stopping ...$INFO_CLOSE"
+		SERVICE="xdg-desktop-portal-wlr"
+		SERVICE_UPDATE
+		$($SCTL_START $SERVICE.service)
+		#sleep 1
+		$SCTL_ACTIVE $SERVICE.service &>/dev/null &&\
+			echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW $($SCTL_ACTIVE $SERVICE.service)$INFO_CHECK" &&\
+			#notify-send
+		SERVICE="xdg-desktop-portal"
+		SERVICE_UPDATE
+		$($SCTL_START $SERVICE.service)
+		#sleep 1
+		$SCTL_ACTIVE $SERVICE.service &>/dev/null &&\
+			echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW $($SCTL_ACTIVE $SERVICE.service)$INFO_CHECK" &&\
+			#notify-send
 		dbus-update-activation-environment --systemd --all
-		
-		
 		;;
 	*)
 		echo "Unknow session"
+		exit 1
 		;;
 esac
+
+## Notifications
+# We want mako as a daemon, not sure why, but looks good
+# Note: If you restart portals, them mako dies! (dont troubleshoot)
+SERVICE="mako"
+SERVICE_UPDATE
+if pgrep "$SERVICE" &>/dev/null; then
+	declare -r MAKO_PROC="1"
+	if [ "$MAKO_PROC" = 1 ]; then
+		echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW Stopping ...$INFO_CLOSE"
+		pkill $SERVICE
+		$($SCTL_ACTIVE $SERVICE.service) &>/dev/null && $($SCTL_STOP $SERVICE.service)
+		echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW Starting ..."
+		$($SCTL_START $SERVICE.service)
+		echo "$SCTL_INFO$SERVICE_INFO$INFO_ARROW $($SCTL_ACTIVE $SERVICE.service)$INFO_CHECK"
+		hyprctl notify -0 10000 "rgb(6AA84F)" "mako --> $($SCTL_ACTIVE mako.service)" &>/dev/null
+	fi
+else
+	$($SCTL_ACTIVE $SERVICE.service) &>/dev/null && $($SCTL_FAILED $SERVICE.service) &&\
+		$($SCTL_STOP $SERVICE.service) &&\
+		echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW Stopping ...$INFO_CLOSE"
+	echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW ${magenta}NOTE${default}: Maybe you killed portals?! So..."
+	echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW Starting ..."
+	$($SCTL_START $SERVICE.service)
+	echo "$SCTL_INFO$SERVICE_INFO$INFO_ARROW $($SCTL_ACTIVE $SERVICE.service)$INFO_CHECK"
+	hyprctl notify -0 10000 "rgb(6AA84F)" "mako --> $($SCTL_ACTIVE mako.service)" &>/dev/null
+fi
 
 # Polkit check and restart
 polkit_kde="/usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1"
 polkit_gnome="/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1"
-if [ -f "$polkit_kde" ]; then 
-	pkill -f polkit-kde-auth
-	sleep 2
-	systemctl --user start plasma-polkit-agent.service
+if [ -f "$polkit_kde" ]; then
+	SERVICE="plasma-polkit-agent"
+	SERVICE_UPDATE
+	echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW Stopping ... $INFO_CLOSE"
+	pgrep polkit-kde-auth &>/dev/null && pkill polkit-kde-auth
+	#sleep 1
+	$($SCTL_START $SERVICE.service)
 		if [ "$DESKTOP_SESSION" = hyprland ]; then
-			echo -e "polkid-kde => $(systemctl --user is-active plasma-polkit-agent.service)"
-			hyprctl notify -1 10000 "rgb(ff1ea3)" "polkid-kde => $(systemctl --user is-active plasma-polkit-agent.service)"
+			$SCTL_ACTIVE $SERVICE.service &>/dev/null &&\
+				echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW $($SCTL_ACTIVE $SERVICE.service)$INFO_CHECK" &&\
+				$($NOTIFY "$HYPR_INFO$HYPR_SERVICE -> $($SCTL_ACTIVE $SERVICE.service)")
+				#hyprctl notify -0 10000 "rgb(6AA84F)" "$HYPR_INFO$HYPR_SERVICE -> $($SCTL_ACTIVE $SERVICE.service)" &>/dev/null
 		elif [ "$DESKTOP_SESSION" = sway ]; then
-			echo -e "polkid-kde => $(systemctl --user is-active plasma-polkit-agent.service)"
-			notify-send -u low -t 10000 "polkid-kde => $(systemctl --user is-active plasma-polkit-agent.service)"
+			$SCTL_ACTIVE $SERVICE.service &>/dev/null &&\
+				echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW $($SCTL_ACTIVE $SERVICE.service)$INFO_CHECK"
+				$($NOTIFY "$HYPR_INFO$HYPR_SERVICE -> $($SCTL_ACTIVE $SERVICE.service)")
 		fi
 fi
 if [ -f "$polkit_gnome" ];then
@@ -61,36 +154,19 @@ if [ -f "$polkit_gnome" ];then
 fi
 
 # Audio restart for integration
-pipewire=( "wireplumber.service" "pipewire.service" "pipewire.socket" )
-for service in "${pipewire[@]}"; do
-	if systemctl is-active --quiet --user "$service"; then
-		if [ "$DESKTOP_SESSION" = hyprland ]; then
-			echo -e "$service => $(systemctl --user is-active $service)"
-			hyprctl notify -1 10000 "rgb(ff1ea3)" "$service --> $(systemctl --user is-active $service)"
-		elif [ "$DESKTOP_SESSION" = sway ]; then
-			echo -e "$service => $(systemctl --user is-active $service)"
-			notify-send -u low -t 10000 "$service --> $(systemctl --user is-active $service)"
-		fi
+# NOTE: Keep order in list for avoiding integration issue
+audio=( "wireplumber" "pipewire-pulse" "pipewire-alsa" "pipewire" )
+for service in "${audio[@]}"; do
+	SERVICE="$service"
+	SERVICE_UPDATE
+	if pgrep "$SERVICE" &>/dev/null; then
+		echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW $SERVICE is restarting ..."
+		$($SCTL_RESTART $SERVICE)
+		echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW $($SCTL_ACTIVE $SERVICE.service)$INFO_CHECK"
 	else
-		if [ "$DESKTOP_SESSION" = hyprland ]; then
-			echo -e "$service => $(systemctl --user is-active $service)"
-			hyprctl notify -0 10000 "rgb(ff1ea3)" "$service --> $(systemctl --user is-active $service)"
-		elif [ "$DESKTOP_SESSION" = sway ]; then
-			echo -e "$service => $(systemctl --user is-active $service)"
-			notify-send -u critical -t 10000 "$service --> $(systemctl --user is-active $service)"
-		fi
+		echo -e "$SCTL_INFO$SERVICE_INFO$INFO_ARROW $SERVICE not found! $INFO_CLOSE"
 	fi
 done
 
 # Start waybar if session is sway
 [ "$DESKTOP_SESSION" = sway ] && waybar -c $HOME/.config/waybar/config-sway &
-
-## Notifications
-# We want mako as a daemon, not sure why, but looks good
-systemctl --user is-active mako.service && systemctl --user stop mako.service
-pgrep mako 2>/dev/null && pkill mako
-systemctl --user start mako.service
-echo "mako: => $(systemctl --user is-active mako.service)"
-hyprctl notify -0 10000 "rgb(ff1ea3)" "mako --> $(systemctl --user is-active mako.service)"
-
-##notify-send -u critical "$service --> $(systemctl --user is-active $service)"
