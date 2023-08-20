abc3e41b692813936882af8b039e2ffba96f9ae3
tuxietux83 <66544011+tuxietux83@users.noreply.github.com>
2023-08-20 21:08:46 +0300

    Update install_debian_hypr.sh
    make it smaller

--- install_debian_hypr.sh
+++ install_debian_hypr.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
-set -e -u
+set -e
+ninjameson="1" # 0->config only, 1->config->build->install
 ## List of apps and libs
 # Apps that you desire
 apps=(
+	[title]="apps"
 	apt-file
 	mlocate
 	mako-notifier
@@ -26,22 +28,26 @@ apps=(
 )
 # Drivers (intel for me)
 drivers=(
+	[title]="drivers"
 	intel-media-va-driver
 	vainfo
 )
 # Some fonts
 fonts=(
+	[title]="fonts"
 	fonts-jetbrains-mono
 	fonts-font-awesome
 	xfonts-terminus	
 )
 # Network manager and tray icon
 network=(
+	[title]="network"
 	network-manager-gnome
 	nm-tray
 )
 # Audio, we go with pipewire and wireplumber
 audio=(
+	[title]="audio"
 	pipewire
 	wireplumber
 	pavucontrol
@@ -49,6 +55,7 @@ audio=(
 )
 # Some tweeks
 appeareance=(
+	[title]="appeareance"
 	qtwayland5
 	qt6-wayland
 	gnome-icon-theme
@@ -62,6 +69,7 @@ appeareance=(
 )
 # Base build libs
 build=(
+	[title]="build"
 	build-essential
 	meson
 	ninja-build
@@ -72,11 +80,13 @@ build=(
 )
 # Libdisplay-info libs
 libdisplay_info=(
+	[title]="libdisplay-info"
 	hwdata
 	edid-decode
 )
 # Libinput libs
 libinput=(
+	[title]="libinput"
 	libudev-dev
 	libmtdev-dev
 	libevdev-dev
@@ -87,10 +97,12 @@ libinput=(
 )
 # Libliftoff libs
 libliftoff=(
+	[title]="libliftoff"
 	libdrm-dev
 )
 # Wayland libs
 wayland=(
+	[title]="wayland"
 	libxml2-dev
 	graphviz
 	doxygen
@@ -98,9 +110,11 @@ wayland=(
 )
 # Wayland-protocols libs
 wayland_protocols=(
+	[title]="wayland-protocols"
 )
 # Wlroots libs
 wlroots=(
+	[title]="wlroots"
 	glslang-tools
 	libgbm-dev
 	libavutil-dev
@@ -118,16 +132,19 @@ wlroots=(
 )
 # XDG-desktop-portal-hyprland libs
 xdg_desktop_portal_hyprland=(
+	[title]="xdg-desktop-portal-hyprland"
 	libpipewire-0.3-dev
 	libinih-dev
 	xdg-desktop-portal
 )
 # Hyprland libs
 hyprland=(
+	[title]="hyprland"
 	jq
 )
 # Waybar libs
 waybar=(
+	[title]="waybar"
 	libgtkmm-3.0-dev
 	libdbusmenu-gtk3-dev
 	libjsoncpp-dev
@@ -155,9 +172,12 @@ waybar=(
 	catch2
 )
 nwg_look=(
+	[title]="nwg-look"
 	libwebkit2gtk-4.0-dev
 )
-
+PKG_LIST=( "${apps[*]}" "${drivers[*]}" "${fonts[*]}" "${network[*]}" "${audio[*]}" "${appeareance[*]}" "${build[*]}"\
+		"${libdisplay_info[*]}" "${libinput[*]}" "${libliftoff[*]}" "${wayland[*]}" "${wayland_protocols[*]}" "${wlroots[*]}"\
+		"${xdg_desktop_portal_hyprland[*]}" "${hyprland[*]}" "${waybar[*]}" "${nwg_look[*]}" )
 # Colors
 default=$(tput sgr0)
 black=$(tput setaf 0)
@@ -175,27 +195,13 @@ action="[${green}ACTION${default}]"
 question="[${cyan}QUESTION${default}]"
 info="[${blue}INFO${default}]"
 
+# Check if is running as user
 if [[ $EUID == 0 ]]; then
     echo -e "${info}: ${green}Please dont run as ${red}ROOT${green}!${default}"
     exit 1
 fi
-# Progress function
-show_progress() {
-    for ((i=0; i<10; i++)); do
-        echo -n "${yellow}. ${default}"
-        sleep 0.5
-    done
-    echo " "
-}
-# Call function
-progress() {
-    show_progress &
-    progress_pid=$!
-    sleep 5
-    kill $progress_pid > /dev/null 2>&1
-    echo "${green}Done${default}!"
-}
-clear
+
+# Nala or apt-get
 echo
 echo "${green}What package manager would you like to use${default}:"
 echo
@@ -206,16 +212,17 @@ read -p "${cyan}Select option${default}: " option
 case $option in
 	1)
 		if ! command -v nala &>/dev/null; then
+			sudo apt-get update
 			sudo apt-get install -y nala
 			pkg_mngr="nala"
 			echo -e "${action}: ${yellow}nala${default}: ${green}installed ok${default}!"
 		else
+			sudo apt-get update
 			pkg_mngr="nala"
-			echo -e "${info}: ${yellow}nala${default}: ${green}already installed${default}!"
-			
 		fi
 		;;
 	2)
+		sudo apt-get update
 		pkg_mngr="apt-get"
 		echo -e "${info}: ${yellow}apt-get${default}: ${green}ok${default}!"
 		;;
@@ -230,114 +237,23 @@ sudo "$pkg_mngr" install -y software-properties-common
 sudo apt-add-repository -y non-free contrib
 # Here we go
 clear
+
+### Installing ...
+for installs in "${PKG_LIST[@]}"; do
+var_apps=$(echo "$installs" | cut -d' ' -f1)
+var_install=$(echo "$installs" | cut -d' ' -f2-)
 echo
-echo -e "${info}: ${yellow}We need this for ${green}apps${default}:\n ${yellow}${apps[*]}${default}"
-read -p "${green}Press enter to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
-sudo "$pkg_mngr" install -y "${apps[@]}"
-progress
+echo -e "${info}: ${yellow}We need this for ${green}$var_apps${default}:\n ${default}$installs${default}"
+read -p "${action}: ${green}Press${default} ENTER${green} to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
+sudo "$pkg_mngr" install $var_install
 clear
-echo
-echo -e "${info}: ${yellow}We need this for ${green}drivers${default}:\n ${yellow}${drivers[*]}${default}"
-read -p "${green}Press enter to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
-sudo "$pkg_mngr" install -y "${drivers[@]}"
-progress
-clear
-echo
-echo -e "${info}: ${yellow}We need this for ${green}fonts${default}:\n ${yellow}${fonts[*]}${default}"
-read -p "${green}Press enter to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
-sudo "$pkg_mngr" install -y "${fonts[@]}"
-progress
-clear
-echo
-echo -e "${info}: ${yellow}We need this for ${green}network${default}:\n ${yellow}${network[*]}${default}"
-read -p "${green}Press enter to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
-sudo "$pkg_mngr" install -y "${network[@]}"
-progress
-clear
-echo
-echo -e "${info}: ${yellow}We need this for ${green}audio${default}:\n ${yellow}${audio[*]}${default}"
-read -p "${green}Press enter to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
-sudo "$pkg_mngr" install -y "${audio[@]}"
-progress
-clear
-echo
-echo -e "${info}: ${yellow}We need this for ${green}appeareances${default}:\n ${yellow}${appeareance[*]}${default}"
-read -p "${green}Press enter to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
-sudo "$pkg_mngr" install -y "${appeareance[@]}"
-progress
-clear
-echo
-echo -e "${info}: ${yellow}We need this for ${green}build${default}:\n ${yellow}${build[*]}${default}"
-read -p "${green}Press enter to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
-sudo "$pkg_mngr" install -y "${build[@]}"
-progress
-clear
-echo
-echo -e "${info}: ${yellow}We need this for ${green}libdisplay_info${default}:\n ${yellow}${libdisplay_info[*]}${default}"
-read -p "${green}Press enter to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
-sudo "$pkg_mngr" install -y "${libdisplay_info[@]}"
-progress
-clear
-echo
-echo -e "${info}: ${yellow}We need this for ${green}libinput${default}:\n ${yellow}${libinput[*]}${default}"
-read -p "${green}Press enter to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
-sudo "$pkg_mngr" install -y "${libinput[@]}"
-progress
-clear
-echo
-echo -e "${info}: ${yellow}We need this for ${green}libliftoff${default}:\n ${yellow}${libliftoff[*]}${default}"
-read -p "${green}Press enter to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
-sudo "$pkg_mngr" install -y "${libliftoff[@]}"
-progress
-clear
-echo
-echo -e "${info}: ${yellow}We need this for ${green}wayland${default}:\n ${yellow}${wayland[*]}${default}"
-read -p "${green}Press enter to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
-sudo "$pkg_mngr" install -y "${wayland[@]}"
-progress
-clear
-echo
-echo -e "${info}: ${yellow}We need this for ${green}wayland_protocols${default}:\n ${yellow}${wayland_protocols[*]}${default}"
-read -p "${green}Press enter to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
-sudo "$pkg_mngr" install -y "${wayland_protocols[@]}"
-progress
-clear
-echo
-echo -e "${info}: ${yellow}We need this for ${green}wlroots${default}:\n ${yellow}${wlroots[*]}${default}"
-read -p "${green}Press enter to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
-sudo "$pkg_mngr" install -y "${wlroots[@]}"
-progress
-clear
-echo
-echo -e "${info}: ${yellow}We need this for ${green}XDG_desktop_portal_hyprland${default}:\n ${yellow}${xdg_desktop_portal_hyprland[*]}${default}"
-read -p "${green}Press enter to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
-sudo "$pkg_mngr" install -y "${xdg_desktop_portal_hyprland[@]}"
-progress
-clear
-echo
-echo -e "${info}: ${yellow}We need this for ${green}Hyprland${default}:\n ${yellow}${hyprland[*]}${default}"
-read -p "${green}Press enter to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
-sudo "$pkg_mngr" install -y "${hyprland[@]}"
-progress
-clear
-echo
-echo -e "${info}: ${yellow}We need this for ${green}waybar${default}:\n ${yellow}${waybar[*]}${default}"
-read -p "${green}Press enter to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
-sudo "$pkg_mngr" install -y "${waybar[@]}"
-progress
-clear
+done
+
 echo
 echo -e "${info}: ${yellow}We need this for ${green}SDDM${default} & ${green}xmlto${default}:\n ${yellow}This two sucks ...${default}"
-read -p "${green}Press enter to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
+read -p "${action}: ${green}Press${default} ENTER${green} to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
 sudo "$pkg_mngr" install -y sddm --no-install-recommends
 sudo "$pkg_mngr" install -y xmlto --no-install-recommends
-progress
-clear
-echo
-echo -e "${info}: ${yellow}We need this for ${green}nwg-look${default}:\n ${yellow}${nwg_look[*]}${default}"
-read -p "${green}Press enter to continue or ${red}Ctrl${default}+${red}c ${green}to abort${default} ..."
-sudo "$pkg_mngr" install -y "${nwg_look[@]}"
-progress
 clear
 
 # Making install directory
@@ -348,7 +264,6 @@ else
 	cd install
 	echo -e "${info}: ${yellow}Directory exist ${green}install${default}."
 fi
-progress
 clear
 
 ### LibInput -> Git version (latest)
@@ -360,18 +275,17 @@ echo -e "${action}: ${yellow}If we have a ${green}build${default}. ${yellow}we r
 [ -d build ] && rm -rfv build
 meson setup --prefix=/usr --buildtype=release -Ddocumentation=false build/ &&
 read -p "${question}: ${yellow}Did the ${red}config${yellow} pass${default}? ${yellow}If ${red}not${yellow} press ${red}Ctrl${default}+${red}c ${yellow}to ${red}abort${default}."
-ninja -C build/
-sudo ninja -C build/ install
+[ "$ninjameson" = 1 ] && ninja -C build/
+[ "$ninjameson" = 1 ] && sudo ninja -C build/ install
 # Adding user to group input
 echo -e "${action}: ${yellow}Adding ${green}$USER ${yellow} to ${default}input ${cyan}Group${default}."
 if ! groups $USER | grep &>/dev/null "\binput\b";then
     sudo usermod -a -G input $USER
-    echo -e "${info}: ${yellow}User ${green}$USER${yellow} added to ${default}input ${cyan}Group${default}."
+    echo -e "${action}: ${yellow}User ${green}$USER${yellow} added to ${default}input ${cyan}Group${default}."
 else
     echo -e "${info}: ${yellow}User ${green}$USER${yellow} already is in ${default}input ${cyan}Group${default}."
 fi
 cd ..
-progress
 clear
 
 ### Libdisplay-info -> Git version (latest)
@@ -383,10 +297,9 @@ echo -e "${action}: ${yellow}If we have a ${green}build${default}. ${yellow}we r
 [ -d build ] && rm -rfv build
 meson setup --prefix=/usr --buildtype=release build/ &&
 read -p "${question}: ${yellow}Did the ${red}config${yellow} pass${default}? ${yellow}If ${red}not${yellow} press ${red}Ctrl${default}+${red}c ${yellow}to ${red}abort${default}."
-ninja -C build/ &&
-sudo ninja -C build/ install
+[ "$ninjameson" = 1 ] && ninja -C build/ &&
+[ "$ninjameson" = 1 ] && sudo ninja -C build/ install
 cd ..
-progress
 clear
 
 ### Wayland-Protocols -> Git version (latest)
@@ -398,10 +311,9 @@ echo -e "${action}: ${yellow}If we have a ${green}build${default}. ${yellow}we r
 [ -d build ] && rm -rfv build
 meson setup --prefix=/usr --buildtype=release build/ &&
 read -p "${question}: ${yellow}Did the ${red}config${yellow} pass${default}? ${yellow}If ${red}not${yellow} press ${red}Ctrl${default}+${red}c ${yellow}to ${red}abort${default}."
-ninja -C build/ &&
-sudo ninja -C build/ install
+[ "$ninjameson" = 1 ] && ninja -C build/ &&
+[ "$ninjameson" = 1 ] && sudo ninja -C build/ install
 cd ..
-progress
 clear
 
 ### Wayland -> Git version (latest)
@@ -413,10 +325,9 @@ echo -e "${action}: ${yellow}If we have a ${green}build${default}. ${yellow}we r
 [ -d build ] && rm -rfv build
 meson setup --prefix=/usr --buildtype=release -Ddocumentation=false build/ &&
 read -p "${question}: ${yellow}Did the ${red}config${yellow} pass${default}? ${yellow}If ${red}not${yellow} press ${red}Ctrl${default}+${red}c ${yellow}to ${red}abort${default}."
-ninja -C build/ &&
-sudo ninja -C build install
+[ "$ninjameson" = 1 ] && ninja -C build/ &&
+[ "$ninjameson" = 1 ] && sudo ninja -C build install
 cd ..
-progress
 clear
 
 ### Wlroots and libliftoff -> Git versions (latest)
@@ -430,10 +341,9 @@ echo -e "${action}: ${yellow}If we have a ${green}build${default}. ${yellow}we r
 [ -d subprojects/libliftoff/build ] && sudo rm -rfv subprojects/libliftoff/build
 meson setup --prefix=/usr --buildtype=release build/ &&
 read -p "${question}: ${yellow}Did the ${red}config${yellow} pass${default}? ${yellow}If ${red}not${yellow} press ${red}Ctrl${default}+${red}c ${yellow}to ${red}abort${default}."
-ninja -C build/ &&
-sudo ninja -C build/ install
+[ "$ninjameson" = 1 ] && ninja -C build/ &&
+[ "$ninjameson" = 1 ] && sudo ninja -C build/ install
 cd ..
-progress
 clear
 
 # XDG-desktop-portal-hyprland -> Git version (latest)
@@ -445,10 +355,9 @@ echo -e "${action}: ${yellow}If we have a ${green}build${default}. ${yellow}we r
 [ -d build ] && rm -rf build
 meson setup --prefix=/usr --buildtype=release build/ &&
 read -p "${question}: ${yellow}Did the ${red}config${yellow} pass${default}? ${yellow}If ${red}not${yellow} press ${red}Ctrl${default}+${red}c ${yellow}to ${red}abort${default}."
-ninja -C build/ &&
-sudo ninja -C build/ install
+[ "$ninjameson" = 1 ] && ninja -C build/ &&
+[ "$ninjameson" = 1 ] && sudo ninja -C build/ install
 cd ..
-progress
 clear
 
 #### Hyprland and libliftoff -> Git versions (latest)
@@ -462,22 +371,20 @@ echo -e "${action}: ${yellow}If we have a ${green}build${default}. ${yellow}we r
 [ -d subprojects/libliftoff/build ] && sudo rm -rfv subrpojects/libliftoff/build
 meson setup --prefix=/usr --buildtype=release build/ &&
 read -p "${question}: ${yellow}Did the ${red}config${yellow} pass${default}? ${yellow}If ${red}not${yellow} press ${red}Ctrl${default}+${red}c ${yellow}to ${red}abort${default}."
-ninja -C build/ &&
-sudo ninja -C build install
+[ "$ninjameson" = 1 ] && ninja -C build/ &&
+[ "$ninjameson" = 1 ] && sudo ninja -C build install
 if [ ! -d /usr/share/wayland-sessions ]; then
 	sudo mkdir -p -v /usr/share/wayland-sessions
 	echo -e "${action}: Dir ${green}wayland-sessions ${yellow}created in ${default}/usr/share/"
+	if [ ! -f /usr/share/wayland-sessions/hyprland.desktop ]; then
+		sudo cp -v example/hyprland.desktop /usr/share/wayland-sessions/
+		echo "${action}: ${yellow}Copy ${green}hyprland.desktop ${yellow} to: ${default}/usr/share/wayland-sessions"
+	fi
 else
 	echo -e "${info}: ${yellow}Dir ${green}wayland-session ${yellow} exist in ${default}/usr/share"
-fi
-if [ ! -f /usr/share/wayland-sessions/hyprland.desktop ]; then
-	sudo cp -v example/hyprland.desktop /usr/share/wayland-sessions/
-	echo "${action}: ${yellow}Copy ${green}hyprland.desktop ${yellow} to: ${default}/usr/share/wayland-sessions"
-else
 	echo -e "${info}: ${yellow}File ${green}hyprland.desktop ${yellow}exist in: ${default}/usr/share/wayland-sessions"
 fi
 cd ..
-progress
 clear
 
 # Waybar -> Git version (latest)
@@ -490,15 +397,13 @@ cd Waybar
 echo -e "${action}: ${yellow}If we have a ${green}build${default}. ${yellow}we remove, else we pass ${default}..."
 [ -d build ] && rm -rfv build
 echo -e "${action}: ${yellow}We patch ${default}src/modules/wlr/workspace_manager.cpp ${yellow}for ${green}Hyprland${default}"
-progress
 sed -i -e 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp
 meson setup --prefix=/usr --buildtype=release --auto-features=enabled build &&
 meson configure -Dexperimental=true build &&
 read -p "${question}: ${yellow}Did the ${red}config${yellow} pass${default}? ${yellow}If ${red}not${yellow} press ${red}Ctrl${default}+${red}c ${yellow}to ${red}abort${default}."
-ninja -C build &&
-sudo ninja -C build install
+[ "$ninjameson" = 1 ] && ninja -C build &&
+[ "$ninjameson" = 1 ] && sudo ninja -C build install
 cd ..
-progress
 clear
 
 # nwg-look -> Git version (latest)
@@ -507,16 +412,16 @@ echo -e "${action}: ${yellow}Cloning repository and entering it ${default}..."
 [ ! -d nwg-look ] && git clone https://github.com/nwg-piotr/nwg-look.git
 cd nwg-look
 make build
+echo -e "${info}:${yellow} Dont't panic, this will take a while${default} ..."
 read -p "${question}: ${yellow}Did the ${red}build${yellow} pass${default}? ${yellow}If ${red}not${yellow} press ${red}Ctrl${default}+${red}c ${yellow}to ${red}abort${default}."
-sudo make install
+[ "$ninjameson" = 1 ] && sudo make install
 cd ../..
-progress
 clear
 
 # CleanUp
 echo -e "${action}: ${yellow}We remove ${default}install ${yellow}directory${default}"
-sleep 4
-[ -d install ] && sudo rm -rfv install
+sleep 2
+[ -d install ] && rm -rfv install
 clear
 
 ### Post install
@@ -555,30 +460,29 @@ echo -e "${info}: ${yellow}Your original configs will be backed up in${default}:
 read -p "${cyan}Proceed${default}: ${green}Y${default}/${green}y ${default}or ${red}N${default}/${red}n${default}: " backup
 case $backup in
 	Y|y)
-		echo -e "${info}: ${yellow}BackingUp${default}: $HOME/.config/backup/${date_tag}"
-		mkdir -pv "$HOME/.config/backup/${date_tag}"
-		source_dir="dot/config"
-		dest_dir="$HOME/.config/backup/${date_tag}"
-			if [ -d "$HOME/.config/systemd" ]; then
-				exclude_dir="dot/config/systemd"
-			else
-				exclude_dir=""
+	source_dir="dot/config"
+	dest_dir="$HOME/.config/backup/${date_tag}"
+	echo -e "${info}: ${yellow}BackingUp${default}: $HOME/.config/backup/${date_tag}"
+	mkdir -pv "$HOME/.config/backup/${date_tag}"
+		if [ -d "$HOME/.config/systemd" ]; then
+			exclude_dir="systemd"
+		else
+			exclude_dir=""
+		fi
+	directories=($(ls -1 -I "$exclude_dir" "$source_dir"))
+		for dir in "${directories[@]}"; do
+    		mv_dir="$HOME/.config/$dir"
+    		if [ -d "$mv_dir" ];then
+				mv -v "$mv_dir" "$dest_dir"
 			fi
-		directories=($(find "$source_dir" -maxdepth 1 -type d -not -path "$exclude_dir" | tail -n +2))
-			for dir in "${directories[@]}"; do
-				dir_name=$(basename "$dir")
-    				mv_dir="$HOME/.config/${dir_name}"
-    				if [ -d "$mv_dir" ];then
-					mv -v "$mv_dir" "$dest_dir"
-     				fi
-				rsync -av "$source_dir/" "$HOME/.config"
-			done
-		;;
+		done
+	[ -d $HOME/.config/systemd ] && rsync -av --exclude "$source_dir/systemd" "$source_dir/" "$HOME/.config"
+	[ ! -d $HOME/.config/systemd ] && rsync -av "$source_dir/" "$HOME/.config"
+	;;
 	N|n)
-		exit 1
-		;;
+	echo -e "${action} ${yellow} Nothing to do ${default}."
+	;;
 	*)
-		echo -e "${info}: ${red}Invalid option${default}!"
-		exit 1
-		;;
+	echo -e "${info}: ${red}Invalid option${default}!"
+	;;
 esac
